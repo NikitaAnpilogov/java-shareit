44495f9deed1c2cb8a4aae0210e02824a3981aaf
NikitaAnpilogov <nikanp8@gmail.com>
2025-08-27 22:47:05 +0300

    Убрал методы, которые не используются

--- server/src/main/java/ru/practicum/shareit/booking/mapper/BookingMapper.java
+++ server/src/main/java/ru/practicum/shareit/booking/mapper/BookingMapper.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.booking.mapper;
 
 import ru.practicum.shareit.booking.dto.BookingDto;
-import ru.practicum.shareit.booking.dto.BookingShortDto;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.item.mapper.ItemMapper;
 import ru.practicum.shareit.user.mapper.UserMapper;
